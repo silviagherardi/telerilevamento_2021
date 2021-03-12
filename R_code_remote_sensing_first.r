4a009969e3b5f1768a4dd84d6ea9ed2aee32e183
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-03-12 11:00:14 +0100

    Create R_code_remote_sensing_first.r

---
+++ R_code_remote_sensing_first.r
@@ -0,0 +1,10 @@
+# Il mio primo codice in R per il telerilevamento!
+install.packages(“raster”)
+library(raster) 
+
+setwd("C:/lab/") # percorso per Windows
+
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+p224r63_2011
+
+plot(p224r63_2011)
