17f74d143d9ba9658d4ff38c9efb0266ee8e280b
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-03-17 12:21:39 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,10 +1,13 @@
 # Il mio primo codice in R per il telerilevamento!
-install.packages(“raster”)
+# installare il pacchetto raster per gestire i dati in formato raster
+install.packages("raster") 
+# funzione library per visualizzare il pacchetto raster
 library(raster) 
-
-setwd("C:/lab/") # percorso per Windows
-
+# percorso per Windows per lavorare con la cartella lab
+setwd("C:/lab/") 
+# funzione brick per importare il blocco di immagini satellitari e assegnare un oggetto 
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
+# vedere cosa contiene il file
 p224r63_2011
-
+# funzione plot per visualizzare le 7 bande
 plot(p224r63_2011)
