7f8ae9a9d2919f2c207388549bdde12d5ceb6989
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-03-21 14:37:45 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -3,17 +3,21 @@
 install.packages("raster") 
 # funzione library per visualizzare il pacchetto raster
 library(raster) 
+
 # percorso per Windows per lavorare con la cartella lab
 setwd("C:/lab/") 
+
 # funzione brick per importare il blocco di immagini satellitari e assegnare un oggetto 
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 # vedere cosa contiene il file
 p224r63_2011
 # funzione plot per visualizzare le 7 bande
 plot(p224r63_2011)
-# funzione per cambiare colore e assegnamo un oggetto
+
+# funzione per cambiare colore e assegnamo la funzioena all'oggetto cl
 cl <- colorRampPalette(c("black","grey","light grey")) (100)
+# funzione plot -> primo argomento:immagine, secondo argomento:colore
 plot(p224r63_2011, col=cl)
 # esercizio change colour -> new
-cl <- colorRampPalette(c("blue","pink","light pink","purple","green")) (100)
-plot(p224r63_2011, col=cl)
+clb <- colorRampPalette(c("blue","pink","light pink","purple","green")) (100)
+plot(p224r63_2011, col=clb)
