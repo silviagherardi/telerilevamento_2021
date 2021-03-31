6d4ad3f1287adb8e1a8b9c19f78e6c659acd562c
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-03-31 13:03:34 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,7 +1,8 @@
 # TIME SERIES ANALYSIS
 # Analizziamo l'incremento della temperatura in Groenlandia
 # Data and code from Emanuela Cosma
+
 # install.packages("raster")
 library(raster)
-# percordo Windows per lavorare con la cartella greenland che si trova dentro la cartella lab
+# percorso Windows per lavorare con la cartella greenland che si trova dentro la cartella lab
 setwd("C:/lab/greenland") 
