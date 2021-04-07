de932c8eab921e61843984e94f850e332825a58d
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-04-07 12:35:28 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -7,5 +7,32 @@ library(raster)
 # percorso Windows per lavorare con la cartella greenland che si trova dentro la cartella lab
 setwd("C:/lab/greenland") 
 
+# funzione RASTER: serve per caricare singoli dati, in questo caso singoli strati (diversa dalla funzione brick che importa un intero pacchetto di dati)
+# associamo il risultato della funzione all'oggetto lst_2000 (nome dell'immagine)
+lst_2000 <- raster("lst_2000.tif")
+# plottiamo l'immagine lst_2000 per vedere cosa contiene
+plot(lst_2000)
+# immagine del 2005
+lst_2005 <- raster("lst_2005.tif")
+plot(lst_2005)
+# immagine del 2010
+lst_2010 <- raster("lst_2010.tif")
+plot(lst_2010)
+# immagine del 2015
+lst_2015 <- raster("lst_2015.tif")
+plot(lst_2015)
+
+# richiamiamo l'immagine per vedere alcune informazioni
+lst_2005
+# values: 0, 65535  (min, max) -> immagine a 16 bit, 2^16 = 65.356 valori possibili
+
+# creiamo un multipanel con le 4 immagini in un quadrato 2x2
+par(mfrow=c(2,2)
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+
 
 
