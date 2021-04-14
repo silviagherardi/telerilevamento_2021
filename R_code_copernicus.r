2a4d6604fae112a044d8468b3947c7644d727fcc
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-04-14 12:29:41 +0200

    Create R_code_copernicus.r

---
+++ R_code_copernicus.r
@@ -0,0 +1,9 @@
+# R_code_copernicus.r
+# Visualizing Copernicus data
+
+# definiamo le librerie che utilizziamo
+library(raster)
+install.packages("ncdf4")
+library(ncdf4) 
+
+# settiamo la working directory
