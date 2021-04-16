5c3b7e05b7a6040eaa740283db5657eba4bdda2c
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-04-16 10:27:17 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -3,6 +3,19 @@
 # settiamo la working directory
 setwd("C:/lab/")
 
-# installare il pacchetto knitr: serve per fare un report
+# richiamiamo tutte le library
+# pacchetto knitr: può utilizzare un codice esterno (code.r) per creare un report
 install.packages("knitr") 
 library(knitr)
+library (raster)
+library (rasterVis)
+# installare un altro pezzo di R per creare il report
+install.packages("tinytex")
+library(tinytex)
+
+# prendiamo la repository su Greenland - copia e incolla sul blocco note
+# salvare il file (blocco note) nella cartella lab con il nome: R-code-Greenland.r
+# dunque cambiamo l'estensione da .txt a .r
+# funzione stitch
+# primo argomento: nome del nostro codice tra "" (R-code-Greenland.r) 
+stitch("R-code-Greenland.r", template=system.file("misc","knitr-template.Rnw", package="knitr"))
