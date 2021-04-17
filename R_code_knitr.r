49b954e2ad4bb6ac686413887cc81e5ca3840560
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-04-17 21:25:22 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -3,24 +3,24 @@
 # settiamo la working directory
 setwd("C:/lab/")
 
-# richiamiamo tutte le library
+# richiamiamo tutte le library necessarie
+library (raster)
+library (rasterVis)
 # pacchetto knitr: può utilizzare un codice esterno (code.r) per creare un report
 install.packages("knitr") 
 library(knitr)
-library (raster)
-library (rasterVis)
 # installare un altro pezzo di R per creare il report
 install.packages("tinytex")
 library(tinytex)
 
-# prendiamo la repository su Greenland - copia e incolla sul blocco note
-# salvare il file (blocco note) nella cartella lab con il nome: R-code-Greenland.r
-# dunque cambiamo l'estensione da .txt a .r
-# funzione stitch
-# primo argomento: nome del nostro codice tra "" (R-code-Greenland.r) 
+# prendiamo la repository sulla Groenlandia, la copiamo e la incolliamo sul blocco note
+# salvare il blocco note (file di testo) nella cartella lab con il nome: R-code-Greenland.r
+# cambiare l'estensione da .txt a .r
+# funzione stitch: crea automaticamente un REPORT basato su uno script di R
+# primo argomento: nome del nostro codice tra "" -> "R-code-Greenland.r"
 stitch("R-code-Greenland.r", template=system.file("misc","knitr-template.Rnw", package="knitr"))
 
 # 2 azioni per risolvere il problema latex 
-# Errore: LaTex to compile R-code-Greenland.tex. 
+# Error: LaTex to compile R-code-Greenland.tex
 tinytex::install_tinytex()
 tinytex::tlmgr_update()
