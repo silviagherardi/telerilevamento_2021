4e3e344824c241915c6bf3e5d3d23f5fb349a5ef
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-04-26 12:06:07 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -1,1 +1,44 @@
 # R_code_multivariate_analysis.r 
+# Analisi multivariata: PCA (dati Landsat, 30m)
+
+# librerie e working directory
+library(raster)
+library(RStoolbox)
+setwd("C:/lab/") 
+
+# funzione brick: carichiamo l'immagine intera costiuita da 7 bande (RasterBrick) 
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+# plottiamo l'immagine per vedere le 7 bande
+plot(p224r63_2011)
+# vediamo le informazioni dell'immagine
+p224r63_2011
+# class: RasterBrick
+# dimensions: 1499, 2967, 4.447.533, 7  (nrow, ncol, ncell, nlayers)
+# resolution: 30, 30  (x, y)
+# extent: 579765, 668775, -522705, -477735  (xmin, xmax, ymin, ymax)
+# crs: +proj=utm +zone=22 +datum=WGS84 +units=m +no_defs 
+# source:    
+# names: B1_sre, B2_sre, B3_sre, B4_sre, B5_sre, B6_bt, B7_sre 
+# min values: 0.000000e+00, 0.000000e+00, 0.000000e+00, 1.196277e-02, 4.116526e-03, 2.951000e+02, 0.000000e+00 
+# max values:    0.1249041,    0.2563655,    0.2591587,    0.5592193,    0.4894984,  305.2000000,    0.3692634 
+
+# correliamo la banda del blu e la banda del verde: dobbiamo plottare la banda del blu contro la banda del verde
+# banda blu: B1_sre -> asse X
+# banda verde: B2_sre -> asse Y
+# $: leghiamo le bande all'immagine completa
+# col="red": colore del plot e in questo modo i punti sono rossi
+# pch=19: point character e in questo modo i punti sono dei cerchietti pieni
+# cex=2: aumentiamo la dimensione dei punti
+plot(p224r63_2011$B1_sre,p224r63_2011$B2_sre, col="red", pch=19, cex=2) 
+# Warning message: In .local(x, y, ...): plot used a sample of 2.2% of the cells. You can use "maxpixels" to increase the sample
+# i pixel da plottare sono oltre 4 milioni, dunque il sistema ci dice che plotta solo il 2.2% di questo totale
+# in statistica il sistema si chiama multicollinearità: le due bande sono molto correlate tra loro e sono correlate positivamente
+# le info del punto sulla X sono molto simili alle info del punto sulla Y
+# spesso questa forma di correlazione è usata in modo causale
+
+# plottiamo di nuovo la stessa immagine ma con B2_sre -> asse X e B1_sre -> asse Y 
+plot(p224r63_2011$B2_sre,p224r63_2011$B1_sre, col="red", pch=19, cex=2)
+
+# funzione pairs: serve per plottare tutte le correlazione possibili tra tutte le variabili di un dataset
+# mette in correlazione a due a due tutte le variabili di un certo dataset
+pairs(p224r63_2011)
