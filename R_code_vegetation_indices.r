d3300dd7813a06b8a4da3d76a20cf6de6ec90c1f
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-04-28 12:57:30 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -1,3 +1,16 @@
 # R_code_vegetation_indices.r
 # librerie:
 library(raster)
+# working directory:
+setwd("C:/lab/") 
+
+# funzione brick: importiamo le due immagini dentro R
+defor1 <- brick("defor1.jpg")
+defor2 <- brick("defor2.jpg")
+# b1 = NIR; b2 = red; b3 = green 
+
+# parmfrow 
+# plotRGB
+par(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
