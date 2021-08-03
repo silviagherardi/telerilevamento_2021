5b009865f9e1fc75d0a84911ef991bd84be4ee4b
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-08-03 14:41:01 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -1,4 +1,4 @@
-# R_code_multivariate_analysis.r 
+# R_code_multivariate_analysis.r
 # Analisi multivariata: PCA (dati Landsat, 30m)
 # usiamo una PCA per compattare il sistema in un numero inferiore di bande ma conservando la stessa proporzione
 
