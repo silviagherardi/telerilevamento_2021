33553574682cfe0583937c68717655fd07308f93
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-11-19 11:41:15 +0100

    Update R_code_complete.r

--- R_code_complete.r
+++ R_code_complete.r
@@ -577,6 +577,7 @@ plot(albedores, col=cl)
 # 4. R code knitr 
 
 # R_code_knitr.r
+# Vogliamo mettere all'interno di uno stesso PDF tante immagini e creare un report (codice + immagini) 
 
 # settiamo la working directory
 setwd("C:/lab/")
@@ -584,21 +585,23 @@ setwd("C:/lab/")
 # richiamiamo tutte le library necessarie
 library (raster)
 library (rasterVis)
-# pacchetto knitr: può utilizzare un codice esterno (code.r) per creare un report
+# pacchetto knitr per creare un report
 install.packages("knitr") 
 library(knitr)
+# il pacchetto knitr può utilizzare un codice esterno (code.r) per creare un record
+
 # installare un altro pezzo di R per creare il report
 install.packages("tinytex")
 library(tinytex)
 
-# prendiamo la repository sulla Groenlandia: la copiamo e la incolliamo sul blocco note
+# Codice che vogliamo utilizzare: prendiamo la repository sulla Groenlandia, la copiamo (control A) e la incolliamo (control V) sul blocco note
 # salviamo il blocco note (file di testo) nella cartella lab con il nome: R-code-Greenland.r
 # cambiamo l'estensione da .txt a .r
 # usiamo il pacchetto knitr che utilizza il codice appena salvato nella cartella lab, lo carica dentro a R e poi genera il report
 
-# funzione stitch: crea automaticamente un REPORT basato su uno script di R
+# funzione stitch: crea automaticamente un REPORT basato su uno script di R (Github) 
 # primo argomento: nome del codice che abbiamo salvato nella cartella lab tra "" -> "R-code-Greenland.r"
-# "misc": il tipo di template all'interno di knitr
+# template: il tipo di template all'interno di knitr è il "misc"
 # "knit-template.Rnw": il file di riferimento 
 # "knitr": il pacchetto che si utilizza 
 stitch("R-code-Greenland.r", template=system.file("misc","knitr-template.Rnw", package="knitr"))
