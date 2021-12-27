fee195fa0c4f1d8073aa9b846c2ade7a62d1657a
silviagherardi <80513568+silviagherardi@users.noreply.github.com>
2021-12-27 14:18:24 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -1,100 +1,51 @@
 # R_code_exam.r
-# PAPUA-INDONESIA 1990-2020
+# World of Change: manto nevoso in Sierra Nevada
 
-# Sito utilizzato per scaricare le immagini: https://eros.usgs.gov/image-gallery/earthshot/papua-indonesia
-# Luogo di studio: Papua (provincia dell'Indonesia) 
+
+# Sito utilizzato per scaricare le immagini: https://earthobservatory.nasa.gov/world-of-change/SierraNevada
+# Luogo di studio: Sierra Nevada (California) 
 
 # LIBRERIE E WORKING DIRECTORY
 # Imposto le librerie necessarie per le indagini
 # install.packages("raster")
-library(raster) # per gestire i dati in formato raster 
+library(raster) # per gestire i dati in formato raster e le funzioni associate 
 # install.packages("RStoolbox) 
-library(RStoolbox) 
+library(RStoolbox) # 
 
 # Settare la working directory
 setwd("C:/esame_telerilevamento_2021/")
 
-# Satellite LANDSAT 5 -> immagini 1990 - 2007  
-# BANDE: 
-#   -5: Swir
-#   -4: Nir
-#   -3: Red
 
-# Satellite LANDSAT 8 -> immagini 2015 - 2020
-#   -6: Swir 
-#   -5: Nir
-#   -4: Red
 # ------------------------------------------------------------------------------------------------------------------------
 
 # INTRODUZIONE - Caricare le immagini - Visualizzare le immagini e le relative informazioni
 
-# Importo tutte le immagini:
 # funzione brick: serve per importare dentro a R l'intera immagine satellitare costituita da tutte le sue singole bande (intero set di bande)
-papua1990 <- brick("1_11-20-1990_Papua_Main(chs. 5,4,3).png")
-papua2007 <- brick("3_1-19-2007_Papua_Main(chs. 5,4,3).png")
-papua2015 <- brick("7_9-22-2015_Papua_Main(chs. 6,5,4).png")
-papua2020 <- brick("8-18-2020_Papua_Main(chs_6,5,4).png")
+sn2006 <- brick("sierranevada_tmo_200685.jpg")
+sn2015 <- brick("sierranevada_tmo_201590.jpg")
 
-# Leggo le informazioni di ogni file RasterBrick: 
-papua1990
+#
+sn2006
 # class      : RasterBrick 
-# dimensions : 869, 1000, 869000, 4  (nrow, ncol, ncell, nlayers)
+# dimensions : 720, 720, 518400, 3  (nrow, ncol, ncell, nlayers)
 # resolution : 1, 1  (x, y)
-# extent     : 0, 1000, 0, 869  (xmin, xmax, ymin, ymax)
+# extent     : 0, 720, 0, 720  (xmin, xmax, ymin, ymax)
 # crs        : NA 
-# source     : C:/esame_telerilevamento_2021/1_11-20-1990_Papua_Main(chs. 5,4,3).png 
-# names      : X1_11.20.1990_Papua_Main.chs._5.4.3..1, X1_11.20.1990_Papua_Main.chs._5.4.3..2, X1_11.20.1990_Papua_Main.chs._5.4.3..3, X1_11.20.1990_Papua_Main.chs._5.4.3..4 
-# min values :                                      0,                                      0,                                      0,                                      0 
-# max values :                                    255,                                    255,                                    255,                                    255
+# source     : C:/esame_telerilevamento_2021/sierranevada_tmo_200685.jpg 
+# names      : sierranevada_tmo_200685.1, sierranevada_tmo_200685.2, sierranevada_tmo_200685.3 
+# min values :                         0,                         0,                         0 
+# max values :                       255,                       255,                       255 
 
-papua2007
+sn2015
 # class      : RasterBrick 
-# dimensions : 869, 1000, 869000, 4  (nrow, ncol, ncell, nlayers)
+# dimensions : 720, 720, 518400, 3  (nrow, ncol, ncell, nlayers)
 # resolution : 1, 1  (x, y)
-# extent     : 0, 1000, 0, 869  (xmin, xmax, ymin, ymax)
+# extent     : 0, 720, 0, 720  (xmin, xmax, ymin, ymax)
 # crs        : NA 
-# source     : C:/esame_telerilevamento_2021/3_1-19-2007_Papua_Main(chs. 5,4,3).png 
-# names      : X3_1.19.2007_Papua_Main.chs._5.4.3..1, X3_1.19.2007_Papua_Main.chs._5.4.3..2, X3_1.19.2007_Papua_Main.chs._5.4.3..3, X3_1.19.2007_Papua_Main.chs._5.4.3..4 
-# min values :                                     0,                                     0,                                     0,                                     0 
-# max values :                                   255,                                   255,                                   255,                                   255 
-
-papua2015
-# class      : RasterBrick 
-# dimensions : 869, 1000, 869000, 4  (nrow, ncol, ncell, nlayers)
-# resolution : 1, 1  (x, y)
-# extent     : 0, 1000, 0, 869  (xmin, xmax, ymin, ymax)
-# crs        : NA 
-# source     : C:/esame_telerilevamento_2021/7_9-22-2015_Papua_Main(chs. 6,5,4).png 
-# names      : X7_9.22.2015_Papua_Main.chs._6.5.4..1, X7_9.22.2015_Papua_Main.chs._6.5.4..2, X7_9.22.2015_Papua_Main.chs._6.5.4..3, X7_9.22.2015_Papua_Main.chs._6.5.4..4 
-# min values :                                     0,                                     0,                                     0,                                     0 
-# max values :                                   255,                                   255,                                   255,                                   255
-
-papua2020
-# class      : RasterBrick 
-# dimensions : 869, 1000, 869000, 4  (nrow, ncol, ncell, nlayers)
-# resolution : 1, 1  (x, y)
-# extent     : 0, 1000, 0, 869  (xmin, xmax, ymin, ymax)
-# crs        : NA 
-# source     : C:/esame_telerilevamento_2021/8-18-2020_Papua_Main(chs_6,5,4).png 
-# names      : X8.18.2020_Papua_Main.chs_6.5.4..1, X8.18.2020_Papua_Main.chs_6.5.4..2, X8.18.2020_Papua_Main.chs_6.5.4..3, X8.18.2020_Papua_Main.chs_6.5.4..4 
-# min values :                                  0,                                  0,                                  0,                                  0 
-# max values :                                255,                                255,                                255,                                255 
-
-# le 4 immagini sono dei RasterBrick e ognuna di queste è composta da 4 bande
-# 869.000 pixel per ciascuna banda
-# immagini a 8 bit -> 2^8 = 256 -> 0-255 valori 
-
-
-# funzione plot: serve per visualizzare i dati, in questo caso visualizziamo tutte le 7 bande dell'intera immagine satellitare
-plot(papua1990)
-plot(papua2020) 
-
-
-
-
-
-
-
+# source     : C:/esame_telerilevamento_2021/sierranevada_tmo_201590.jpg 
+# names      : sierranevada_tmo_201590.1, sierranevada_tmo_201590.2, sierranevada_tmo_201590.3 
+# min values :                         0,                         0,                         0 
+# max values :                       255,                       255,                       255 
 
 
 
